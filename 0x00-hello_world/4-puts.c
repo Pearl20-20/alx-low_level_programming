a5e372318fe12e35bcf3f9d136256a01d5bddbc7
Pearl20-20 <manneshie@yahoo.com>
2022-12-08 15:16:38 -0800

    task 4 compiled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #!/bin/bash
-#include <stio.h>
+#include <stdio.h>
 
 /**
  * main - prints the phrase "programming is like building a multilingual puzzle 
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
