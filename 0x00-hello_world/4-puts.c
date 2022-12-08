4758c8ca96ba63e21f9b6deb764caf2a77091f31
Pearl20-20 <manneshie@yahoo.com>
2022-12-08 14:26:36 -0800

    4-puts.c'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stio.h>
+
+/**
+ *
+ * main - prints the phrase "programming is like building a multilingual puzzle 
+ * Return: 0 if exited properly, non-zero ortherwise
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n);
+	return(0);
+}
