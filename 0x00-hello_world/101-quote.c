01e428b8e0cf552585f5e2d511d46cc290fa6cab
Pearl20-20 <manneshie@yahoo.com>
2022-12-08 18:25:18 -0800

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints exactly and that piece of art
- * is useful" - Dora Korpar, 2015-10-19
- * Return: 1 (success) always
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	write(1, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (0);
 }
