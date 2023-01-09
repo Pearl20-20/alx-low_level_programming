f3cdcd66968f1197fbdffd7b6013ca16ba2bff80
Pearl20-20 <manneshie@yahoo.com>
2023-01-09 13:57:52 -0800

    problem fixed

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @c: the character to be printed
+ *
+ * Return: 0n succes 1
+ * and on error -1 is returned and the error is set appropriatelly
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1);
+}
