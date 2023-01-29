975e0aa41ac5f22d407b1178a3d2e935a4b58323
Pearl20-20 <manneshie@yahoo.com>
2023-01-29 07:14:45 -0800

    101-mul.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * op_add - This function returns the sum of a and b
  * @a: The first int to add
  * @b: The second int to add
  * Return: The integer sum of both a and b
- */
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
  * op_sub - This function returns the difference of a and b
  * @a: The first difference operator
@@ -17,8 +20,9 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
+
 /**
  * op_mul - This function returns the product of a and b
  * @a: The first int to multiply
@@ -27,8 +31,9 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
+
 /**
  * op_div - This function returns the result of the division of a by b
  * @a: The int dividend
@@ -38,8 +43,9 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
+
 /**
  * op_mod - This function returns the remainder of the division of a by b
  * @a: The int dividend
